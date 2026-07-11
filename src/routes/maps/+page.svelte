79011c9b9f433649f7ad1c0e8bf5a3896832a94e
Paige Andrews <pmandrews@gmail.com>
2026-07-11 14:49:17 -0400

    Fix image asset paths

--- src/routes/maps/+page.svelte
+++ src/routes/maps/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import Italy from "$lib/maps/Fantasy Italy.png"
-  import Town from "$lib/maps/Little Lamplight.png"
+  import italyMap from "./images/Fantasy Italy.png"
+  import townMap from "./images/Little Lamplight.png"
 </script>
 
 <svelte:head>
@@ -11,15 +11,15 @@
 
 <p>The town of Little Lamplight sits just east of the Republic of Venice and just west of the Ottoman empire, at the northernmost tip of the Adriatic Sea.</p>
 
-<enhanced:img src={Italy} alt="Map of 1400s Italy showing the fictional location of Little Lamplight" />
+<enhanced:img src="./images/Fantasy Italy.png" alt="Map of 1400s Italy showing the fictional location of Little Lamplight" />
 
-<p><a href={Italy} class="underline text-blue-900">Open full-size Italy map</a></p>
+<p><a href={italyMap} class="underline text-blue-900">Open full-size Italy map</a></p>
 
 <p>There is a small fictional inlet called the Adriatic Bay which cuts through the middle of Little Lamplight.</p>
 
-<enhanced:img src={Town} alt="A map of the town of Little Lamplight" />
+<enhanced:img src="./images/Little Lamplight.png" alt="A map of the town of Little Lamplight" />
 
-<p><a href={Town} class="underline text-blue-900" >Open full-size Little Lamplight map</a></p>
+<p><a href={townMap} class="underline text-blue-900" >Open full-size Little Lamplight map</a></p>
 
 <p>The Wayward, indicated by <b>&#128971;</b>, is just north of the Adratic Bay.</p>
 
