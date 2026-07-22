c40c67f2ad13c7adb4cd88e92ebfe1fb6d1c5584
Paige Andrews <pmandrews@gmail.com>
2026-07-21 20:59:41 -0400

    Add more house rules from first session explanations

--- src/routes/rules/+page.svelte
+++ src/routes/rules/+page.svelte
@@ -4,33 +4,71 @@
 
 <h1>House Rules</h1>
 
-Little Lamplight has a few new rules to keep in mind:
+<p>Little Lamplight has a few new rules to keep in mind:</p>
 
 <h2 id="changeling-lore">New Merit: Changeling Lore</h2>
 
-Specific knowledge about seeming’s, Kith’s, and courts.
+<p>Specific knowledge about seeming’s, Kith’s, and courts.</p>
 
 <ol class="my-4 rating">
-<li>You understand Seemings and Kiths. Pledges and Oaths and why they are necessary but dangerous. You understand how glamour is harvested, clarity and why it is important, the Mask and its purpose with mortals. You have knowledge of what contracts your own seeming and kith can provide as well as universal contracts.</li>
-<li>You understand the purpose of the courts and constant change. You are familiar with Seeming and Kith contracts outside of your own.</li>
-<li>You are aware there are different court structures out there and how they work. You are aware of Entitlements.</li>
-<li>You know the abilities of different courts, and of specific Entitlements.</li>
+  <li>You understand Seemings and Kiths. Pledges and Oaths and why they are necessary but dangerous. You understand how glamour is harvested, clarity and why it is important, the Mask and its purpose with mortals. You have knowledge of what contracts your own seeming and kith can provide as well as universal contracts.</li>
+  <li>You understand the purpose of the courts and constant change. You are familiar with Seeming and Kith contracts outside of your own.</li>
+  <li>You are aware there are different court structures out there and how they work. You are aware of Entitlements.</li>
+  <li>You know the abilities of different courts, and of specific Entitlements.</li>
 </ol>
 
 <h2 id="hedge-lore">New Merit: Hedge Lore</h2>
 
-Specific knowledge about the hedge, this merit cannot be purchased past 1 dot without contracts of the dream (pathfinder).
+<p>Specific knowledge about the hedge, this merit cannot be purchased past 1 dot without contracts of the dream (pathfinder).</p>
+
+<p>Each point of Hedge Lore adds +1 die to all Survival rolls made within the Hedge.</p>
 
 <ol class="my-4 rating">
-<li>Knowledge of hedge gates, Trod’s and basic theories of the hedge</li>
-<li>Basic knowledge of the local hedge layout, basic hedge fruits, hobs, and hedge beasts, goblin contracts.</li>
-<li>You know where the local hedge market is and how to get in. Specific knowledge about local hobs, hedge fruit, hedge beasts. Advanced knowledge of the local hedge including shortcuts and dangerous areas.</li>
-<li>Expert knowledge of the local hedge, including many secrets the average person will miss.</li>
-<li>Knowledgeable enough to attempt to predict the changes of the hedge before they happen.</li>
+  <li>Knowledge of hedge gates, Trod’s and basic theories of the hedge</li>
+  <li>Basic knowledge of the local hedge layout, basic hedge fruits, hobs, and hedge beasts, goblin contracts.</li>
+  <li>You know where the local hedge market is and how to get in. Specific knowledge about local hobs, hedge fruit, hedge beasts. Advanced knowledge of the local hedge including shortcuts and dangerous areas.</li>
+  <li>Expert knowledge of the local hedge, including many secrets the average person will miss.</li>
+  <li>Knowledgeable enough to attempt to predict the changes of the hedge before they happen.</li>
 </ol>
 
 <h2>Changed Merits: Contacts &amp; Allies</h2>
 
-<p>
-The Contacts and Allies merits may be taken more than once. Each instance of the merit represents a specific person or group with whom the character is affiliated, and the strength of that connection.
-</p>
+<p>The Contacts and Allies merits may be taken more than once. Each instance of the merit represents a specific person or group with whom the character is affiliated, and the strength of that connection.</p>
+
+<h2 id="court-mantle">Changed Merit: Court Mantle</h2>
+
+<p>Court Mantle cannot be bought with XP. It is granted after ST review and takes into account kudos from other players for roleplaying the court.</p>
+
+<h2>Stunt Dice</h2>
+
+<p>You can get extra dice on rolls by describing the action you're taking using the five senses: sight, smell, touch, taste, and hearing. Describing one sense gives +1 die, three senses grants +2, and describing all five grant +3 dice to the roll.</p>
+
+<h2>Oaths</h2>
+
+<p>Mundane oaths and promises are binding, and can be sworn without expending Glamour.</p>
+
+<p>All active oaths and pledges are visible within a character's Mien, though the parties to that oath and its clauses are not.</p>
+
+<h2>Pledges</h2>
+
+<p>The various Hollow merits _can_ be purchased with the merit dots granted by a Pledge. This is an exception to the pledge boon rule that disallows purchasing supernatural merits.</p>
+
+<h2 id="hollows">Hollow Merits</h2>
+
+<p>The Hollow merit works a bit differently than by book. Instead of a single merit with individual dots dedicated to specific features, each hollow is defined by a suite of merits which may be purchased once for that hollow.</p>
+
+<p><b>Hollow:</b> the basic merit. This must have at least one dot in order to be an owner of the hollow. The dots in Hollow cap the maximum rating of all other hollow-related merits.</p>
+
+<p><b>Garden:</b> Produces one generic Healing or Glamour goblin fruit per dot, which can be gathered each session. The types gathered can be chosen each session.</p>
+
+<p><b>Size:</b> How big the hollow is.</p>
+
+<p><b>Security:</b> How hard the hollow is to find and breach from the Hedge. Having zero dots in Security risks incursion each session.</p>
+
+<p><b>Doors:</b> Additional entrances to the hollow. Every hollow automatically has a door in the human world and the door in the hedge. Each point in Doors adds a single new entry point, which may be in either world. Doors placed on moving objects (like a cart or a boat) only function while that object is within the borders of Little Lamplight.</p>
+
+<p><b>Workshop:</b> Tools and equipment for pursuing a craft while within the hedge.</p>
+
+<p><b>Archive:</b> Dedicated space for storing, finding, and reading magical tomes. They might appear there on their own, too.</p>
+
+<p><b>Amenities:</b> Magical conveniences that the hollow provides. Like an ice chest that supplies fresh food each day, or a rejuvenating pool that eases aches and pains.</p>
